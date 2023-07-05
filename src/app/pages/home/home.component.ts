757b5f99233b5a751e83291774efdc81fa8f2886
Jefferson Souza <jefferson.souza.correa@gmail.com>
2023-07-04 21:50:36 -0300

    style(Home): layout improvements

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MomentService } from 'src/app/services/moment/moment.service';
+import { IMoment } from 'src/app/shared/models/moment';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  faSearch = faSearch;
 
+  allMoments: IMoment[] = [];
+  moments: IMoment[] = [];
+  baseApiUrl = environment.baseApiUrl;
+
+  constructor(private momentService: MomentService) {}
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((item) => {
+      const data = item.data;
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'en-US'
+        );
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
--- src/app/services/moment/moment.service.ts
+++ src/app/services/moment/moment.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IMoment } from 'src/app/shared/models/moment';
+import { IResponse } from 'src/app/shared/models/response';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,9 +10,13 @@ import { environment } from 'src/environments/environment';
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl;
-  private apiUrl = `${this.baseApiUrl}/moments`;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<IResponse<IMoment[]>> {
+    return this.http.get<IResponse<IMoment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
